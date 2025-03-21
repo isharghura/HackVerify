986f24105997a4fd76cd9df5b478f29980b7d577
isharghura <ishar.ghuras@gmail.com>
2025-03-20 21:40:39 -0400

    feat: scrape project page for github link

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ all_project_links = []
 pg_num = 1
 
 while True:
+    print(f"getting all projects on page {pg_num}")
 
     proj_gallery_url = f"{test_url}/project-gallery?page={pg_num}"
 
     result = requests.get(proj_gallery_url)
     gallery = BeautifulSoup(result.text, "html.parser")
 
+    # project link location
     curr_project_links = gallery.find_all("a", class_="block-wrapper-link fade link-to-software")
 
     if not curr_project_links:
+        print(f"no projects on page {pg_num}")
         break
 
     for link in curr_project_links:
@@ -23,5 +26,21 @@ while True:
 
     pg_num+=1
 
-for link in all_project_links:
-    print(link)
+all_github_links = []
+
+for project_link in all_project_links:
+    result = requests.get(project_link)
+    project = BeautifulSoup(result.text, "html.parser")
+
+    # find github repo link
+    for a_tag in project.find_all("a", href=True):
+        if "github.com" in a_tag["href"]:
+            github_link = a_tag["href"]
+            break
+
+    if github_link:
+        print(f"found {github_link} from {project_link}")
+        all_github_links.append(github_link)
+    else:
+        print("couldn't find github link")
+        all_github_links.append(f"no github link found for: {project_link}")
