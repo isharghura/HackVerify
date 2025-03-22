ba12bfe2d61e24088be75bbdfb8e50e8109507bb
isharghura <ishar.ghuras@gmail.com>
2025-03-22 12:36:40 -0400

    fix: get around FUNCTION_INVOCATION_FAILED error

--- api/scrape.py
+++ api/scrape.py
@@ -1,61 +1,76 @@
+from http.server import BaseHTTPRequestHandler
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
-def handler(event, context):
-    test_url = "https://cuhacking6.devpost.com"
-    all_project_links = []
-    pg_num = 4
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
 
-    # scrape through devpost to find all projects
-    while True:
-        print(f"getting all projects on page {pg_num}")
+            test_url = "https://cuhacking6.devpost.com"
+            all_project_links = []
+            pg_num = 1
 
-        proj_gallery_url = f"{test_url}/project-gallery?page={pg_num}"
-        result = requests.get(proj_gallery_url)
-        gallery = BeautifulSoup(result.text, "html.parser")
+            # scrape through devpost to find all projects
+            try:
+                print(f"getting all projects on page {pg_num}")
 
-        # project link location
-        curr_project_links = gallery.find_all(
-            "a", class_="block-wrapper-link fade link-to-software"
-        )
+                proj_gallery_url = f"{test_url}/project-gallery?page={pg_num}"
+                result = requests.get(proj_gallery_url)
+                gallery = BeautifulSoup(result.text, "html.parser")
 
-        if not curr_project_links:
-            print(f"no projects on page {pg_num}")
-            break
+                # project link location
+                curr_project_links = gallery.find_all(
+                    "a", class_="block-wrapper-link fade link-to-software"
+                )
 
-        for link in curr_project_links:
-            all_project_links.append(link["href"])
+                for link in curr_project_links:
+                    all_project_links.append(link["href"])
 
-        pg_num += 1
+                pg_num += 1
 
-    all_github_links = []
+            except Exception as e:
+                self.wfile.write(f"could not get projects: {str(e)}".encode())
+                return
 
-    github_link = None
+            all_github_links = []
 
-    # scrape through each project
-    for project_link in all_project_links:
-        github_link = None
-        result = requests.get(project_link)
-        project = BeautifulSoup(result.text, "html.parser")
+            # scrape through each project
+            for project_link in all_project_links:
+                github_link = None
+                try:
+                    result = requests.get(project_link)
+                    project = BeautifulSoup(result.text, "html.parser")
 
-        # find github repo link
-        for a_tag in project.find_all("a", href=True):
-            if "github.com" in a_tag["href"]:
-                github_link = a_tag["href"]
-                break
+                    # find github repo link
+                    for a_tag in project.find_all("a", href=True):
+                        if "github.com" in a_tag["href"]:
+                            github_link = a_tag["href"]
+                            break
 
-        if github_link:
-            print(f"found {github_link} from {project_link}")
-            all_github_links.append(github_link)
-        else:
-            print("couldn't find github link")
-            all_github_links.append(f"no github link found for: {project_link}")
+                    if github_link:
+                        print(f"found {github_link} from {project_link}")
+                        all_github_links.append(github_link)
+                    else:
+                        print("couldn't find github link")
+                        all_github_links.append(
+                            f"no github link found for: {project_link}"
+                        )
+                except Exception as e:
+                    all_github_links.append(
+                        f"could not scrape {project_link}: {str(e)}"
+                    )
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/plain",
-        },
-        "body": str(all_github_links),
-    }
+            self.wfile.write(str(all_github_links).encode())
+            return
+
+        except Exception as e:
+            self.send_response(500)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(f"server error: {str(e)}".encode())
+            return
